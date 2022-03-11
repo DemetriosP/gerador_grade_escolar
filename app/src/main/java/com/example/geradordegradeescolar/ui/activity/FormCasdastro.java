e2a8e1039bfaefc014c648388351d1c399a61767
DemetriosP <pantaleao15@hotmail.com>
2022-03-10 21:15:12 -0300

    Implementação parcial do FormCadastro

--- app/src/main/java/com/example/geradordegradeescolar/ui/activity/FormCasdastro.java
+++ app/src/main/java/com/example/geradordegradeescolar/ui/activity/FormCasdastro.java
@@ -2,6 +2,9 @@ package com.example.geradordegradeescolar.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,22 +14,37 @@ import com.example.geradordegradeescolar.R;
 public class FormCasdastro extends AppCompatActivity {
 
     private TextView entrar;
+    private EditText senha, confirmarSenha, nome, email;
+    private Button criarConta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_casdastro);
         iniciarComponentes();
+        vaiTelaLogin();
 
+    }
+
+    private void vaiTelaLogin() {
         entrar.setOnClickListener(v -> {
             Intent intent = new Intent(FormCasdastro.this, FormLogin.class);
             startActivity(intent);
             finish();
         });
-
     }
 
-    private void iniciarComponentes(){
+    private void iniciarComponentes() {
         entrar = findViewById(R.id.textEntrar);
+        senha = findViewById(R.id.editSenha);
+        criarConta = findViewById(R.id.btCadastrese);
+        confirmarSenha = findViewById(R.id.editConfirmarSenha);
+        nome = findViewById(R.id.editNome);
+        email = findViewById(R.id.editEmail);
     }
+
+    private boolean comprarSenhas(EditText senha, EditText confirmarSenha) {
+        return senha.equals(confirmarSenha);
+    }
+
 }
